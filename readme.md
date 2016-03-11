8a1cefa29bc84f69cdf2c467f6b81f45f3914ff2
shane F <shane@jobrapp.com>
2016-03-10 19:14:21 -0800

    server actually does something now

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Go Templates By Example
--- server.go
+++ server.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var (
+	fsDir       = http.FileServer(http.Dir(filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "enahs", "gotemplatesbyexample", "assets")))
+	tmplatesDir = filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "enahs", "gotemplatesbyexample", "templates")
 )
 
 func main() {
+
+	http.Handle("/assets/", http.StripPrefix("/assets/", fsDir))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("go templates by example"))
+		index := filepath.Join(tmplatesDir, "/index.html.tmpl")
+		t, err := template.ParseFiles(index)
+		if err != nil {
+			w.Write([]byte("could not parse template!" + err.Error()))
+		}
+		t.Execute(w, nil)
 	})
 	http.ListenAndServe(":8080", nil)
 }
